4fc938c4818652518d403df7e2c9f70ccb4eb675
Davi Alexandre <davi@davialexandre.com.br>
2019-08-09 22:28:47 -0300

    PCHR-4473: Set CiviCRM 5.15.2 as the default for CiviHR

--- src/civibuild.aliases.sh
+++ src/civibuild.aliases.sh
@@ -74,7 +74,7 @@ function civibuild_alias_resolve() {
     bmaster)     SITE_TYPE=backdrop-demo    ; CIVI_VERSION=master    ; CMS_TITLE="CiviCRM Sandbox on Backdrop"       ;;
     b-master)    SITE_TYPE=backdrop-demo    ; CIVI_VERSION=master    ; CMS_TITLE="CiviCRM Sandbox on Backdrop"       ;;
 
-    civihr)      SITE_TYPE=civihr           ; CIVI_VERSION=5.12.0     ; CMS_TITLE="CiviHR Sandbox"                   ; NO_SAMPLE_DATA=1       ;;
+    civihr)      SITE_TYPE=civihr           ; CIVI_VERSION=5.15.1    ; CMS_TITLE="CiviHR Sandbox"                   ; NO_SAMPLE_DATA=1       ;;
 
     bempty)      SITE_TYPE=backdrop-empty   ; CIVI_VERSION=none      ; CMS_TITLE="Backdrop Sandbox"                  ;;
     dempty)      SITE_TYPE=drupal-empty     ; CIVI_VERSION=none      ; CMS_TITLE="Drupal 7 Sandbox"                  ;;
